11ffe642ac06539fa354a47b2d87d6edc5c4ba6c
eduardoleon19 <eduardo.leon.b3@gmail.com>
2023-06-23 13:51:06 -0500

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return(
+    <div className=' w-20 m-8'>
+      <p className=' font-bold text-orange-700'>Hola</p>
+      <p className=' text-green-600'>Hola, que tal?</p>
+    </div>
+  )
+}
+
+export default App
